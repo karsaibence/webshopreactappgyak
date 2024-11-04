c5a98ff6c1593a76b7c20cf329a08d9f77fa3ea6
karsaibence <karesz333@hotmail.com>
2024-11-04 15:38:16 +0100

    véglegesített, jó megoldás

--- src/components/kartya/Kartya.jsx
+++ src/components/kartya/Kartya.jsx
@@ -1,31 +1,34 @@
-import React, { useContext } from 'react'
-import "./kartya.css"
-import { KosarContext } from '../../context/KosarbaTesz'
+import React, { useContext } from "react";
+import "./kartya.css";
+import { KosarContext } from "../../context/KosarbaTesz";
 
 const Kartya = (props) => {
-    const { getKosarItems } = useContext(KosarContext);
+  const { getKosarItems } = useContext(KosarContext);
 
-    function clickEvent() {
-        getKosarItems(props.obj)
-    }
+  function clickEvent() {
+    getKosarItems(props.obj);
+  }
 
-    return (
-        <div className='card'>
-            <div className="card-header">
-                <h4>{props.obj.name}</h4>
-            </div>
-            <div className="card-img">
-                <img src={"./forras/" + props.obj.url} alt={props.obj.name} />
-            </div>
-            <div className="card-description">
-                <p>{props.obj.leiras}</p>
-                <div className='kosarba-tesz'>
-                    <button onClick={clickEvent}>Vásárlás</button>
-                    <h6>Ár: {props.obj.price}Ft</h6>
-                </div>
-            </div>
+  return (
+    <div className="card">
+      <div className="card-header">
+        <h4>{props.obj.name}</h4>
+      </div>
+      <div className="card-img">
+        <img
+          src={`${process.env.PUBLIC_URL}/forras/${props.obj.url}`}
+          alt={props.obj.name}
+        />
+      </div>
+      <div className="card-description">
+        <p>{props.obj.leiras}</p>
+        <div className="kosarba-tesz">
+          <button onClick={clickEvent}>Vásárlás</button>
+          <h6>Ár: {props.obj.price}Ft</h6>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Kartya
+export default Kartya;
